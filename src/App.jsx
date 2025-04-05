7fb6e67ee9a629e5d70d3505e225294e3a75a772
fabricyoc <fazin_c@hotmail.com>
2025-04-05 11:54:49 -0300

    Cards

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+
+  return (
+    <>
+      <h1>Copa do Mundo Feminina</h1>
+
+      <section className='cards'>
+        <Card />
+      </section>
+    </>
+  )
+}
+
+export default App
