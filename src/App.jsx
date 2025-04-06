bef8d82994814ae94d04f7a1c537c6ef4c1c7e83
fabricyoc <fazin_c@hotmail.com>
2025-04-06 11:10:35 -0300

    Classificação por grupos com filter e map

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Card from './components/Card'
 import GameTable from './components/GameTable'
+import GroupStandings from './components/GroupStandings'
 
 function App() {
 
@@ -15,6 +16,12 @@ function App() {
       <h2>Tabela de Jogos</h2>
       <section className='gameTable'>
         <GameTable />
+      </section>
+
+      <h2>Classificação por grupo</h2>
+      <section className='groupTable'>
+        <h2>Grupo A</h2>
+        <GroupStandings />
       </section>
     </>
   )
