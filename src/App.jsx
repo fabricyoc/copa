f1c8501efcac3f79f4a9c8658db8c73bdfd6a05c
fabricyoc <fazin_c@hotmail.com>
2025-04-12 10:15:31 -0300

    Navegação por abas

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,58 @@
+import { useState } from 'react'
 import './App.css'
 import TournamentBracket from './components/TournamentBracket'
 
 function App() {
+
+  const [activeTab, setActiveTab] = useState('Tab 1');
+
+  function handleChangeTab(tabName) {
+    setActiveTab(tabName);
+  }
+
+  function renderTabContent() {
+    switch (activeTab) {
+      case 'Tab 1':
+        return <TournamentBracket fase='semifinais' />;
+      case 'Tab 2':
+        return <TournamentBracket fase='quartas' />;
+      case 'Tab 3':
+        return <TournamentBracket fase='oitavas' />;
+      default:
+        break;
+    }
+  }
 
   return (
     <>
       <h1>Copa do Mundo Feminina</h1>
 
       <section className='knockoutTable'>
+        <div className='tabs'>
+          <button
+            className={activeTab === 'Tab 1' ? 'active' : ''}
+            onClick={() => handleChangeTab('Tab 1')}
+          >
+            Semifinais
+          </button>
+          <button
+            className={activeTab === 'Tab 2' ? 'active' : ''}
+            onClick={() => handleChangeTab('Tab 2')}
+          >
+            Quartas
+          </button>
+          <button
+            className={activeTab === 'Tab 3' ? 'active' : ''}
+            onClick={() => handleChangeTab('Tab 3')}
+          >
+            Oitavas
+          </button>
+        </div>
 
-        <h2>Finais</h2>
-        <TournamentBracket fase='finais' />
+        <div className='tab_content'>
+          {renderTabContent()}
+        </div>
 
-        <h2>Semifinais</h2>
-        <TournamentBracket fase='semifinais' />
-
-        <h2>Quartas</h2>
-        <TournamentBracket fase='quartas' />
-
-        <h2>Oitavas</h2>
-        <TournamentBracket fase='oitavas' />
       </section>
     </>
   )
